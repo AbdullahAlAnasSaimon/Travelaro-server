b17aaf33b0a8fcbd95963c4b822087e11a9afbb1
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-11-08 18:21:28 +0600

    required some fake data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+// const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,9 +10,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const services = require('./data/services.json');
+
 app.get('/', (req, res) =>{
   res.send('Travelaro server is running');
 })
+
+app.get('/services', (req, res) =>{
+  res.send(services);
+})
+
 
 app.listen(port, () => {
   console.log('Travelaro server is running on port:', port);
