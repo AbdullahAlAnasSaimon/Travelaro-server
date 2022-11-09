2fbee8c9c9ed2a6daeb1444acd4f7a7124dd2fd5
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-11-09 11:04:52 +0600

    get data from server

--- index.js
+++ index.js
@@ -11,26 +11,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const services = require('./data/services.json');
-const reviews = require('./data/reviews.json');
-
 app.get('/', (req, res) =>{
   res.send('Travelaro server is running');
 })
 
-
+// setup up mongodb 
 const uri = `mongodb+srv://${process.env.TRAVELARO_USER}:${process.env.USER_PASSWORD}@cluster0.jt8oxuk.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
   try{
     const serviceCollection = client.db('travelaroDbUser').collection('services');
-
+    
+    // services post request
     app.post('/services', async(req, res) =>{
       const serviceData = req.body;
-      // console.log(serviceData);
       const result = await serviceCollection.insertOne(serviceData);
+      res.send(result);
+    })
+
+    // services get request
+    app.get('/services', async(req, res) =>{
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
       res.send(result);
     })
   }
@@ -39,26 +43,6 @@ async function run(){
   }
 }
 run().catch(err => console.log(err));
-
-app.get('/services', (req, res) =>{
-  res.send(services);
-})
-
-app.get('/reviews', (req, res) =>{
-  res.send(reviews);
-})
-
-app.get('/services/:id', (req, res) =>{
-  const id = parseInt(req.params.id);
-  const signleService = services.find(service => service.id === id)
-  res.send(signleService);
-})
-
-app.get('/reviews/:id', (req, res) =>{
-  const id = req.params.id;
-  const review = reviews.filter(r => r.serviceId == id);
-  res.send(review);
-})
 
 app.listen(port, () => {
   console.log('Travelaro server is running on port:', port);
