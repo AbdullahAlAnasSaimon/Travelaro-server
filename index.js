4f94b105ee1005f06ed5cd94e2a0c683d0fdf948
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-11-10 11:09:14 +0600

    set update reveiw api

--- index.js
+++ index.js
@@ -20,14 +20,15 @@ const uri = `mongodb+srv://${process.env.TRAVELARO_USER}:${process.env.USER_PASS
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next){
+
   const authHeader = req.headers.authorization;
   if(!authHeader){
-    return res.status(401).send({message: 'Unauthorized access'})
+    return res.status(401).send({message: 'Unauthorized access', status: 401})
   }
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
     if(err){
-      return res.status(401).send({message: 'Unauthorized access'});
+      return res.status(401).send({message: 'Unauthorized access', status: 401});
     }
     req.decoded = decoded;
     next();
@@ -42,18 +43,18 @@ async function run(){
 
     app.post('/jwt', (req, res) =>{
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'});
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '20d'});
       res.send({token});
     })
     
-    // services post request
+    // services post api
     app.post('/services', async(req, res) =>{
       const serviceData = req.body;
       const result = await serviceCollection.insertOne(serviceData);
       res.send(result);
     })
 
-    // services get request
+    // services get api
     app.get('/services', async(req, res) =>{
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -77,20 +78,14 @@ async function run(){
       res.send(result);
     })
 
-    // post request for review
-    app.post('/reviews', async(req, res) =>{
-      const reviewData = req.body;
-      const result = await reviewCollection.insertOne(reviewData);
-      res.send(result);
-    })
-
-    
     // query with email
-    app.get('/reviews', verifyJWT, async(req, res) =>{
-      const decoded = req.decoded;
-      if(decoded.email !== req.query.email){
-        res.status(403).send({message: "Unauthorized access"});
+    app.get('/reviews', verifyJWT, async (req, res) =>{
+      const decode = req.decoded;
+
+      if(decode.email !== req.query.email){
+        return res.status(403).send({message: "Unauthorized access", status: 403});
       }
+
       let query = {};
       if(req.query.email){
         query = {
@@ -103,23 +98,44 @@ async function run(){
     })
     
     // get request for multiple review data
-    app.get('/reviews', async(req, res) =>{
-      console.log(req.query);
-      const query = {};
-      const cursor = reviewCollection.find(query);
-      const result = await cursor.toArray()
+    app.get('/reviews/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
+    })
+
+    // post api for review
+    app.post('/reviews', async(req, res) =>{
+      const reviewData = req.body;
+      const result = await reviewCollection.insertOne(reviewData);
       res.send(result);
     })
     
-    // get request for single review data
+    // get api for single review data
     app.get('/reviews/:id', async(req, res) =>{
       const id = req.params.id;
       const query = {serviceId: id};
-      const cursor = reviewCollection.find(query);
+      const cursor = reviewCollection.find(query).sort({insertTime: -1});
       const result = await cursor.toArray();
       res.send(result);
     })
 
+    // update review api
+    app.put('/reviews/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: ObjectId(id)};
+      const option = {upsert: true};
+      const updateReview = {
+        $set: {
+          description: req.body.description
+        }
+      }
+      const result = await reviewCollection.updateOne(filter, updateReview, option);
+      res.send(result);
+    })
+
+    // delete reveiw api
     app.delete('/reviews/:id', async(req, res) => {
       const id = req.params.id;
       const query = {_id: ObjectId(id)};
